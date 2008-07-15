418b304d3a5f351e3c8bc609122b6d5e991542c1
Rasmus Andersson <rasmus@spotify.com>
2008-07-15 21:30:12 +0200

    Repos list not moving along with window resize anymore. Thanks Mattias.

--- app/EYMainSplitviewDelegate.m
+++ app/EYMainSplitviewDelegate.m
@@ -2,13 +2,15 @@
 
 @implementation EYMainSplitviewDelegate
 
+#define MIN_WIDTH 100.0
+#define MAX_WIDTH 400.0
 
 - (CGFloat)splitView:(NSSplitView *)sender constrainMinCoordinate:(CGFloat)proposedMin ofSubviewAt:(NSInteger)offset {
-  return 100.0;
+  return MIN_WIDTH;
 }
 
 - (CGFloat)splitView:(NSSplitView *)sender constrainMaxCoordinate:(CGFloat)proposedMax ofSubviewAt:(NSInteger)offset {
-  return 400.0;
+  return MAX_WIDTH;
 }
 
 - (NSRect)splitView:(NSSplitView *)splitView additionalEffectiveRectOfDividerAtIndex:(NSInteger)dividerIndex {
@@ -18,6 +20,17 @@
                     left_frame.size.height-dragger_size.height,
                     dragger_size.width,
                     dragger_size.height);
+}
+
+- (void)splitView:(NSSplitView *)sender resizeSubviewsWithOldSize:(NSSize)oldSize {
+  NSView *v0 = [[splitview subviews] objectAtIndex:0];
+  NSRect v0frame = [v0 frame];
+  v0frame.size.height = [sender frame].size.height;
+  if (v0frame.size.width > MAX_WIDTH)
+    v0frame.size.width = MAX_WIDTH;
+  else if (v0frame.size.width < MIN_WIDTH)
+    v0frame.size.width = MIN_WIDTH;
+  [v0 setFrame:v0frame];
 }
 
 
